ae93cf1c4a86038d8e2f0c6869ebd4aa4fcd41ed
michaelrhcp <michaelrhcp@hotmail.com>
2018-03-06 21:23:05 -0300

    First commit, building backend

---
+++ fantasy-backend-mvc5/Fantasy-Backend-mvc5/Fantasy-Backend-mvc5/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Fantasy_Backend_mvc5.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ fantasy-backend-mvc5/Fantasy-Backend-mvc5/Fantasy-Backend-mvc5/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Fantasy_Backend_mvc5.Startup))]
+
+namespace Fantasy_Backend_mvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
