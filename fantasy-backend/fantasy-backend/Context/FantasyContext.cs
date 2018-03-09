75bc30e4cf31783b9087fe83cfc1f8903333ce07
michaelrhcp <michaelrhcp@hotmail.com>
2018-03-08 22:23:21 -0300

    Models e Context criados

---
+++ fantasy-backend/fantasy-backend/Context/FantasyContext.cs
@@ -0,0 +1,50 @@
+﻿using fantasy_backend.Models;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace fantasy_backend.Context
+{
+    public class FantasyContext : DbContext
+    {
+        public FantasyContext() : base("FantasyConnection") { }
+
+        public DbSet<Participante> Participantes { get; set; }
+
+        public DbSet<Etapa> Etapas { get; set; }
+
+        public DbSet<EtapaParticipante> Etapas_Participantes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<FantasyContext>);
+            Database.SetInitializer<FantasyContext>(null);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+            {
+                Exception raise = dbEx;
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        string message = string.Format("{0}:{1}",
+                                         validationErrors.Entry.Entity.ToString(),
+                                         validationError.ErrorMessage);
+                        raise = new InvalidOperationException(message, raise);
+                    }
+                }
+                throw raise;
+            }
+        }
+    }
+}
---
+++ fantasy-backend/fantasy-backend/Models/EtapaParticipante.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fantasy_backend.Models
+{
+    [Table("Etapas_Participantes", Schema = "Fantasy")]
+    public class EtapaParticipante
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Participante")]
+        public int FKParticipante { get; set; }
+        public virtual Participante Participante { get; set; }
+
+        [ForeignKey("Etapa")]
+        public int FKEtapa { get; set; }
+        public virtual Etapa Etapa { get; set; }
+
+        public double Pontuacao { get; set; }
+    }
+}
