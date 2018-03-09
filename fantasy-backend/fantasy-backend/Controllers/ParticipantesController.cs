9adedaed61ecb232e2fd885d1bd080aca630e748
michaelrhcp <michaelrhcp@hotmail.com>
2018-03-09 01:05:18 -0300

    Controllers feitos

---
+++ fantasy-backend/fantasy-backend/Controllers/ParticipantesController.cs
@@ -0,0 +1,97 @@
+﻿using fantasy_backend.Context;
+using fantasy_backend.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace fantasy_backend.Controllers
+{
+    public class ParticipantesController : ApiController
+    {
+        private FantasyContext db = new FantasyContext();
+
+        [HttpGet]
+        public IQueryable<Participante> GetParticipantes()
+        {
+            return db.Participantes;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetParticipante(int id)
+        {
+            Participante participante = db.Participantes.Find(id);
+            if (participante == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(participante);
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostParticipante(Participante participante)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Participantes.Add(participante);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = participante.Id }, participante);
+        }
+
+        [HttpPut]
+        public IHttpActionResult PutParticipante([FromUri] int id, [FromBody] Participante participante)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != participante.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(participante).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Ok(participante);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteParticipante(int id)
+        {
+            Participante participante = db.Participantes.Find(id);
+            if (participante == null)
+            {
+                return NotFound();
+            }
+
+            db.Participantes.Remove(participante);
+            db.SaveChanges();
+
+            return Ok(participante);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
