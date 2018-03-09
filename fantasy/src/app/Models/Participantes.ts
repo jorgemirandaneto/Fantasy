a35efdc12dedfa299998d329432bc7d3925ddf25
unknown <you@example.com>
2018-03-09 01:00:21 -0300

    Consumo de serviço de participantes[]

---
+++ fantasy/src/app/Models/Participantes.ts
@@ -0,0 +1,5 @@
+export class Participate {
+    Codigo: string;
+    Nome: string;
+    Email: string;
+}
--- fantasy/src/app/service.service.ts
+++ fantasy/src/app/service.service.ts
@@ -1,13 +1,24 @@
 
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Http } from '@angular/http';
+import { Participate } from './Models/Participantes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable()
 export class ServiceService {
 
-  constructor(private http: Http ) { }
-  getParticipante(){
-          
+
+  private HttpUrl = "http://www.mocky.io/v2/5aa204a02f0000cc12d46309";
+
+  constructor(private http: Http) { }
+  getParticipante() {
+    return this.http.get(this.HttpUrl)
+    .map(res => res.json());
   }
-  
 }
