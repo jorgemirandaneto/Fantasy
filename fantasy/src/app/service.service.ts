386cbd92ef32b34872995d0dee7c9abae77a11d0
unknown <you@example.com>
2018-03-08 23:07:35 -0300

    Implementação do service participante

---
+++ fantasy/src/app/service.service.ts
@@ -0,0 +1,13 @@
+
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class ServiceService {
+
+  constructor(private http: Http ) { }
+  getParticipante(){
+          
+  }
+  
+}
