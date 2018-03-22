f03b6441855b00acec0cea0c2da6bc3e78cbb1b4
michaelrhcp <michaelrhcp@hotmail.com>
2018-03-22 10:13:13 -0300

    correção do CORS

--- fantasy-backend/fantasy-backend/App_Start/WebApiConfig.cs
+++ fantasy-backend/fantasy-backend/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace fantasy_backend
@@ -18,7 +19,14 @@ namespace fantasy_backend
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
+            config.EnableCors();
             config.MapHttpAttributeRoutes();
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter); //Remove o formato XML
+            config.Formatters.JsonFormatter.Indent = true; //Retorna os dados no formato JSON
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
