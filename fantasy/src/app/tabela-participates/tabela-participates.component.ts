414be9cd34be74e7ebc53a14e92b9ea6bb4f8f51
jorgemirandaneto <jorgemirandaneto@live.com>
2018-03-26 16:31:35 -0300

    Implementação datatables

--- fantasy/src/app/tabela-participates/tabela-participates.component.ts
+++ fantasy/src/app/tabela-participates/tabela-participates.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ServiceService } from './../service.service';
-import { Participate } from './../Models/Participantes';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ServiceService } from '../service.service';
+import { Router } from '@angular/router';
+import { Participante } from '../Models/Participantes';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-tabela-participates',
@@ -9,18 +10,30 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./tabela-participates.component.css'],
   providers:[ServiceService]
 })
+
+
+
+
 export class TabelaParticipatesComponent implements OnInit {
 
   constructor(private http: ServiceService, private router: Router) { }
 
-  participantes: Participate[] = []  ;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
 
-  ngOnInit() {
-    this.http.getParticipante().subscribe(data => {
-      this.participantes = data;
-    })
+  participantes: Participante[] = []  ;
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      language: {'url': '//cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json'},
+      pagingType: 'full_numbers',
+      pageLength: 2      
   }
+    this.http.getParticipante().subscribe(
+      p => {this.participantes = p; this.dtTrigger.next();})    
+ 
 
+    }
   onNovo(){
     this.router.navigate(["/Participante"]);
   }
